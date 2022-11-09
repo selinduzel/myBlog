241b3b7bae818f337d9b70bda13d3886dccf4159
selinduzel <selintuna92@gmail.com>
2022-11-09 16:37:38 +0300

    9

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,36 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-
+use App\Models\Post;
 use App\Models\Role;
 
 
-
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
-
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
     public function role()
     {
-       return $this->belongsTo(Role::class);
+        return $this->belongsTo(Role::class);
+    }
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
     }
 }
