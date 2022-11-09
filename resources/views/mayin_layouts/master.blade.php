95e18594cdd78636ef99d62af5e2b105afb73ec4
selinduzel <selintuna92@gmail.com>
2022-11-09 12:40:25 +0300

    5

--- resources/views/mayin_layouts/master.blade.php
+++ resources/views/mayin_layouts/master.blade.php
@@ -69,18 +69,25 @@
 						</div>
 						<div class="col-md-10 text-right menu-1">
 							<ul>
-								<li><a href="index.html">Home</a></li>
+								<li><a href="{{route('home')}}">Home</a></li>
 								<li class="has-dropdown">
-									<a href="courses.html">Categories</a>
+									<a href=" #">Categories</a>
 									<ul class="dropdown">
 										<li><a href="#">Programming</a></li>
 										<li><a href="#">Games</a></li>
 										<li><a href="#">Soft Skills</a></li>
 									</ul>
 								</li>
-								<li><a href="about.html">About</a></li>
-								<li><a href="contact.html">Contact</a></li>
-								<li class="btn-cta"><a href="#"><span>Sign in</span></a></li>
+								<li><a href="{{route('about')}}">About</a></li>
+								<li><a href="{{route('contact')}}">Contact</a></li>
+
+								@guest
+								<li class="btn-cta"><a href="{{route('login')}}"><span>Sign in</span></a></li>
+								@endguest
+
+								@auth
+								<li class="btn-cta"><a href="{{route('login')}}"><span>Selin</span></a></li>
+								@endauth
 							</ul>
 						</div>
 					</div>
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+*/ 
 
 Route::get('/', function () {
     return view('home');
@@ -20,5 +20,13 @@ Route::get('/', function () {
 Route::get('/post', function () {
     return view('post');
 })->name('post');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
 
 require __DIR__.'/auth.php';
