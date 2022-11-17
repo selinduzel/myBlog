ded8b998e345b698f07720f772cac248e9c919bf
selinduzel <selintuna92@gmail.com>
2022-11-17 23:51:50 +0300

    18 devam     [17-18 beraber]

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,11 +1,9 @@
 <?php
-
 namespace Database\Seeders;
-
+use App\Models\Comment;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,7 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         Schema::disableForeignKeyConstraints();
         \App\Models\User::truncate();
         \App\Models\Role::truncate();
@@ -23,29 +20,30 @@ class DatabaseSeeder extends Seeder
         \App\Models\Post::truncate();
         \App\Models\Tag::truncate();
         \App\Models\Comment::truncate();
+        \App\Models\Image::truncate();
 
         Schema::enableForeignKeyConstraints();
 
-         \App\Models\Role::factory(1)->create();
-         \App\Models\User::factory(10)->create();
+        \App\Models\Role::factory(1)->create();
+        $users=\App\Models\User::factory(10)->create();
 
-         \App\Models\Category::factory(10)->create();
+        foreach($users as $user){
+            $user->image()->save(\App\Models\Image::factory()->make());
+        }
 
-         $posts = \App\Models\Post::factory(10)->create();
+        \App\Models\Category::factory(10)->create();
+        $posts=\App\Models\Post::factory(10)->create();
+        \App\Models\Comment::factory(10)->create();
+        \App\Models\Tag::factory(10)->create();
+        foreach ($posts as $post){
+            $tags_ids=[];
+            $tags_ids[]=\App\Models\Tag::all()->random()->id;
+            $tags_ids[]=\App\Models\Tag::all()->random()->id;
+            $tags_ids[]=\App\Models\Tag::all()->random()->id;
 
-         \App\Models\Comment::factory(100)->create();
+            $post->tags()->sync($tags_ids);
+            $post->image()->save(\App\Models\Image::factory()->make());
 
-         \App\Models\Tag::factory(10)->create();
-
-         foreach($posts as $post)
-         {
-            $tags_ids = [];
-            $tags_ids[] = \App\Models\Tag::all()->random()->id;
-            $tags_ids[] = \App\Models\Tag::all()->random()->id;
-            $tags_ids[] = \App\Models\Tag::all()->random()->id;
-
-            $post->tags()->sync( $tags_ids);
-         }
-
+        }
     }
 }
